49e6aec5365f2ecbdfcb6efc3f46d8cd0ca29795
1908788a <162087468+1908788a@users.noreply.github.com>
2024-07-27 22:30:55 +0930

    Update function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,7 +1,7 @@
 
 bool is_fanarray(int array[], int n) {
     if (n < 1) {
-        return false;
+        return true;
     }
     int i = 0, j = n - 1;
     while (i < j) {
@@ -13,3 +13,4 @@ bool is_fanarray(int array[], int n) {
     }
     return true;
 }
+
