08dac64e9b64911cfc16912c3aae8c09f787cdc2
1908788a <162087468+1908788a@users.noreply.github.com>
2024-07-27 22:34:32 +0930

    Update main-3-3.cpp

--- main-3-3.cpp
+++ main-3-3.cpp
@@ -6,11 +6,17 @@ double weighted_average(int array[], int n);
 
 int main() {
     // Test data
-    int array[] = {1, 2, 1, 4, 1, 3};
-    int n = 6;
+    int array1[] = {1, 2, 1, 4, 1, 3};
+    int n1 = 6;
+    int array2[] = {1, 1, 1, 1, 1, 1};
+    int n2 = 6;
+    int array3[] = {};
+    int n3 = 0;
 
     // Testing weighted_average function
-    cout << "Weighted average of array: " << weighted_average(array, n) << endl;
+    cout << "Weighted average of array1: " << weighted_average(array1, n1) << endl;
+    cout << "Weighted average of array2: " << weighted_average(array2, n2) << endl;
+    cout << "Weighted average of array3 (empty array): " << weighted_average(array3, n3) << endl;
 
     return 0;
 }
