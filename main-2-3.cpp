e07ba712d50d1533c40c30b038fe4313236b9b88
1908788a <162087468+1908788a@users.noreply.github.com>
2024-07-27 22:28:31 +0930

    Add files via upload

---
+++ main-2-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void two_five_nine(int array[], int n);
+
+int main() {
+    // Test data
+    int array[] = {2, 5, 9, 2, 5, 9, 2, 9};
+    int n = 8;
+
+    // Testing two_five_nine function
+    two_five_nine(array, n);
+
+    return 0;
+}
